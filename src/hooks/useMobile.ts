8b1ee6ad955611d2af6850ce98262c8c66cac661
emmanuel <badejoemmanuel805@gmail.com>
2025-04-03 11:20:10 +0100

    mend

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -5,7 +5,8 @@ const useMobile = () => {
     const BREAK_POINt = 720;
     const [isMobile, setIsMobile] = useState(null);
     useEffect(() => {
-        const mediaQuery = window?.matchMedia(`(max-width: ${BREAK_POINt}px)`);
+        const mediaQuery = window.matchMedia(`(max-width: ${BREAK_POINt}px)`);
+        setIsMobile(window.matchMedia(`(max-width: ${BREAK_POINt}px)`).matches);
         mediaQuery.addEventListener("change", (e) => setIsMobile(e.matches));
         return () =>
             mediaQuery.removeEventListener("change", (e) =>
