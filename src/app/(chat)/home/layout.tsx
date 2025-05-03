a482c14a0ff8917869bdf9f167c016fd9f832b67
Badejo Emmanuel <135185597+professor-12@users.noreply.github.com>
2025-05-03 16:27:13 +0100

    Update layout.tsx

--- src/app/(chat)/home/layout.tsx
+++ src/app/(chat)/home/layout.tsx
@@ -3,11 +3,10 @@ import SocketContext from '@/context/socket-context'
 import TabComponent from '@/context/TabContext'
 import UserContext from '@/context/user-context'
 import React, { FC, ReactNode } from 'react'
-import Component from './Component'
-import { checkAuth } from '@/lib/_server/auth'
+import Component from './Component
 
 const Layout: FC<{ children: ReactNode }> = async (props) => {
-      await checkAuth()
+     
       return (
             <UserContext>
                   <SocketContext>
